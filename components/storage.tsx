b91f855e00a7c21596d56ae02b80126a02a6dd37
sergeizub <admin.webtime@gmail.com>
2025-07-09 21:08:39 +0300

    Update Nanoid

--- components/storage.tsx
+++ components/storage.tsx
@@ -1,6 +1,7 @@
 import { storage } from "@/components/firebasecfg";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { nanoid } from "nanoid";
+
+let nanoid=(t=21)=>crypto.getRandomValues(new Uint8Array(t)).reduce(((t,e)=>t+=(e&=63)<36?e.toString(36):e<62?(e-26).toString(36).toUpperCase():e<63?"_":"-"),"");
 
 export const uploadFile = async (file, folder) => {
   try {
