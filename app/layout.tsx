29b8736d3733a2251a22b594ee32c463213df4a7
sergeizub <admin.webtime@gmail.com>
2025-07-09 15:47:49 +0300

    Update layout.tsx
    Try To Implement firebase SDK

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 
 export const metadata = {
-  title: 'Vercel Blob Starter',
-  description: 'A simple Next.js app with Vercel Blob for image uploads',
+  title: 'Upload Lloyd Photo',
+  description: '',
 }
 
 const inter = Inter({
@@ -19,7 +19,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.variable}>{children}</body>
+      <body className={inter.variable}>{children}
+
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyByd5aLEn7eNAHgCDAu_RLS8dvjd-FL89I",
+    authDomain: "lloyd-photo-uploader.firebaseapp.com",
+    projectId: "lloyd-photo-uploader",
+    storageBucket: "lloyd-photo-uploader.firebasestorage.app",
+    messagingSenderId: "23166346375",
+    appId: "1:23166346375:web:1eb1a1c20420d12f6c0422",
+    measurementId: "G-NNHGXQB9KW"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script> 
+      </body>
     </html>
   )
 }
