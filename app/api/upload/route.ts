937252f290dcc51ea7656244bdb1f0d37c0354aa
sergeizub <admin.webtime@gmail.com>
2025-07-10 13:50:34 +0300

    Add import Storage

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -2,7 +2,7 @@
 import { handleUpload, type HandleUploadBody } from '@vercel/blob/client'
 import { NextResponse } from 'next/server'
 import { useRef, useState } from "react";
-//import { getFile, uploadFile } from "@/components/storage";
+import { getFile, uploadFile } from "@/components/storage";
 
 
 export async function POST(request: Request): Promise<NextResponse> {
